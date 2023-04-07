da49549284616c2bfe723cc941c51f75b75ce667
Carlos-Marte <carlosmartedev@hotmail.com>
2023-04-07 17:13:47 -0400

    Add component structure changes

--- src/components/FollowTwitterCard.jsx
+++ src/components/FollowTwitterCard.jsx
@@ -3,8 +3,9 @@ import { useState } from 'react';
 export const FollowTwitterCard = ({ nickName, name, addArroba }) => {
   const [follow, setFollow] = useState(false);
 
-  const userIcon = `https://unavatar.io/${name}`;
+  const userIcon = `https://unavatar.io/${nickName}`;
   const buttonText = follow ? 'Unfollow' : 'Follow';
+  const buttonClass = follow ? 'is-follow' : 'is-not-follow';
 
   const handleClick = () => {
     setFollow(!follow);
@@ -12,15 +13,17 @@ export const FollowTwitterCard = ({ nickName, name, addArroba }) => {
 
   return (
     <article className='FollowProfileCard'>
-      <img src={userIcon} alt='Profile Icon' className='FollowProfileCard-img' />
-
       <div className='FollowProfileCard-userInfo'>
-        <h1 className='FollowProfileCard-name'>{name}</h1>
-        <p className='FollowProfileCard-nickName'>{addArroba(nickName)}</p>
+        <img src={userIcon} alt='Profile Icon' className='FollowProfileCard-img' />
+
+        <div className='FollowProfileCard-userNickName'>
+          <h1 className='FollowProfileCard-name'>{name}</h1>
+          <p className='FollowProfileCard-nickName'>{addArroba(nickName)}</p>
+        </div>
       </div>
 
       <button
-        className='FollowProfileCard-buttonFollow'
+        className={`FollowProfileCard-buttonFollow ${buttonClass}`}
         onClick={handleClick}
       >
         {buttonText}
